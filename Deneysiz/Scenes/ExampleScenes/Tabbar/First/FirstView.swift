5c5744dfd475200a968674aa16503dd9eb2de531
Ogulcan Keskin <ogulcankeskin93@gmail.com>
2021-06-03 11:23:38 +0300

    add localization

--- Deneysiz/Scenes/ExampleScenes/Tabbar/First/FirstView.swift
+++ Deneysiz/Scenes/ExampleScenes/Tabbar/First/FirstView.swift
@@ -12,12 +12,32 @@ struct FirstView: View {
     @ObservedObject var firstViewModel: FirstViewModel
     
     var body: some View {
-        Text("FirstView")
+        ZStack {
+            Color(UIColor.systemBackground)
+            Text("hello-world")
+            .foregroundColor(Color(UIColor.label))
+        }
     }
 }
 
 struct FirstView_Previews: PreviewProvider {
     static var previews: some View {
-        FirstView(firstViewModel: FirstDependencyContainer().makeFirstViewModel())
+        Group {
+            FirstView(firstViewModel: FirstDependencyContainer().makeFirstViewModel())
+                .preferredColorScheme(.dark)
+                .environment(\.locale, .init(identifier: "tr"))
+                .previewLayout(.fixed(width: 200, height: 200))
+            
+            FirstView(firstViewModel: FirstDependencyContainer().makeFirstViewModel())
+                .preferredColorScheme(.light)
+                .environment(\.locale, .init(identifier: "en"))
+                .previewLayout(.fixed(width: 200, height: 200))
+            
+            FirstView(firstViewModel: FirstDependencyContainer().makeFirstViewModel())
+                .colorScheme(.light)
+                .environment(\.locale, .init(identifier: "en"))
+                .previewLayout(.device)
+        }
     }
+    
 }
